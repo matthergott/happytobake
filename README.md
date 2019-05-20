957c5fdf280ae62a8b25baf8bccc26631018d430
Matt Hergott <matt.c.hergott@gmail.com>
2019-05-20 16:31:47 -0400

    Website Initial Lauch

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# happytobake.github.io
+## About
+
+Simple one page website for @HappyToBake
+
+## Copyright and License
+
+Copyright 2013-2019 Blackrock Digital LLC. Code released under the [MIT](https://github.com/BlackrockDigital/startbootstrap-stylish-portfolio/blob/gh-pages/LICENSE) license.
---
+++ js/script.js
@@ -0,0 +1,27 @@
+const modals = document.getElementsByClassName('modal');
+
+const platterA = document.getElementById("platter-modal-link");
+platterA.onclick = () => modals[0].style.display = "block";
+const cookiesA = document.getElementById("cookies-modal-link");
+cookiesA.onclick = () => modals[1].style.display = "block";
+const muffinsA = document.getElementById("muffins-modal-link");
+muffinsA.onclick = () => modals[2].style.display = "block";
+const otherTreatsA = document.getElementById("other-treats-modal-link");
+otherTreatsA.onclick = () => modals[3].style.display = "block";
+
+const closeButtons = document.getElementsByClassName("modal-close");
+
+for (let i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].onclick = () => {
+        modals[i].style.display = "none";
+    }
+}
+
+window.onclick = (event) => {
+    for (let i = 0; i < modals.length; i++) {
+        if (modals[i] === event.target) {
+            modals[i].style.display = "none";
+            return;
+        }
+    }
+};
